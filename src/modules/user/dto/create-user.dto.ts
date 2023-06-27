d81c3f1b913a7481eef79e0197328fe2446dcbc3
Juvenal Amarante <juvenalamarante@gestartcondominios.com.br>
2023-06-27 10:35:59 -0300

    Adicionando vinculo entre usuarios e departamentos

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -3,6 +3,7 @@ import {
 	IsEmail,
 	IsInt,
 	IsNotEmpty,
+	IsOptional,
 	IsString,
 	Validate,
 } from 'class-validator';
@@ -88,4 +89,17 @@ export class CreateUserDto {
 	@Type(() => Number)
 	@Validate(CargoExists)
 	cargo_id: number;
+
+	@ApiProperty({
+		description: 'Departamentos do usuário',
+		example: [1, 2],
+		required: false,
+	})
+	@IsInt({
+		message:
+			'O campo departamentos informado não é válido. Por favor, forneça um departamento válido.',
+		each: true,
+	})
+	@IsOptional()
+	departamentos: number[];
 }

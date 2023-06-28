0b401091ab5d0a52e11c39bf85ace5088cdecc3d
Juvenal Amarante <juvenalamarante@gestartcondominios.com.br>
2023-06-28 14:37:33 -0300

    Adicionando filtro para listagem de cargos

--- src/modules/ocupations/ocupations.service.ts
+++ src/modules/ocupations/ocupations.service.ts
@@ -4,10 +4,20 @@ import { PrismaService } from 'src/shared/services/prisma.service';
 @Injectable()
 export class OcupationsService {
 	constructor(private readonly prisma: PrismaService) {}
-	async findAllActive() {
+	async findAllActive(busca?: string) {
 		return {
 			success: true,
-			data: await this.prisma.cargo.findMany({ where: { ativo: true } }),
+			data: await this.prisma.cargo.findMany({
+				where: {
+					nome: busca
+						? {
+								contains: busca,
+								mode: 'insensitive',
+						  }
+						: undefined,
+					ativo: true,
+				},
+			}),
 		};
 	}
 }

e05479fd7f9960376441c782565e686278cf7773
Juvenal Amarante <juvenalamarante@gestartcondominios.com.br>
2023-09-12 14:23:18 -0300

    Corrigindo funcoes do servico de notificacoes

--- src/modules/notification-events/notification-events.service.ts
+++ src/modules/notification-events/notification-events.service.ts
@@ -27,12 +27,16 @@ export class NotificationEventsService {
 	findAll(user: UserAuth) {
 		const sala = `${user.id}-${user.empresa_id}`;
 
+		const dataHoje = new Date();
+		const dataLimite = new Date();
+		dataLimite.setDate(dataHoje.getDate() - 2);
+
 		return this.prisma.notificacaoSocket.findMany({
 			select: this.select,
 			where: {
 				sala,
 				created_at: {
-					lt: new Date(),
+					gt: dataLimite,
 				},
 			},
 			orderBy: {
@@ -44,31 +48,43 @@ export class NotificationEventsService {
 	sendNotificationByUser(
 		sendNotificationEventUserDto: SendNotificationEventUserDto,
 	) {
-		return this.notificationWsService.sendUser({
-			usuario_id: sendNotificationEventUserDto.usuario_id,
-			empresa_id: sendNotificationEventUserDto.empresa_id,
-			notification: {
-				titulo: sendNotificationEventUserDto.notification.titulo,
-				conteudo: sendNotificationEventUserDto.notification.conteudo,
-				rota: sendNotificationEventUserDto.notification.rota,
-				data: sendNotificationEventUserDto.notification.data,
-			},
-		});
+		try {
+			return this.notificationWsService.sendUser({
+				usuario_id: sendNotificationEventUserDto.usuario_id,
+				empresa_id: sendNotificationEventUserDto.empresa_id,
+				notification: {
+					titulo: sendNotificationEventUserDto.notification.titulo,
+					conteudo:
+						sendNotificationEventUserDto.notification.conteudo,
+					rota: sendNotificationEventUserDto.notification.rota,
+					data: sendNotificationEventUserDto.notification.data,
+				},
+			});
+		} catch (err) {
+			throw new Error('Ocorreu um erro ao enviar a notificação');
+		}
 	}
 
 	sendNotificationByDepartment(
 		sendNotificationEventDepartmentDto: SendNotificationEventDepartmentDto,
 	) {
-		return this.notificationWsService.sendDepartment({
-			departamento_id: sendNotificationEventDepartmentDto.departamento_id,
-			empresa_id: sendNotificationEventDepartmentDto.empresa_id,
-			notification: {
-				titulo: sendNotificationEventDepartmentDto.notification.titulo,
-				conteudo:
-					sendNotificationEventDepartmentDto.notification.conteudo,
-				rota: sendNotificationEventDepartmentDto.notification.rota,
-				data: sendNotificationEventDepartmentDto.notification.data,
-			},
-		});
+		try {
+			return this.notificationWsService.sendDepartment({
+				departamento_id:
+					sendNotificationEventDepartmentDto.departamento_id,
+				empresa_id: sendNotificationEventDepartmentDto.empresa_id,
+				notification: {
+					titulo: sendNotificationEventDepartmentDto.notification
+						.titulo,
+					conteudo:
+						sendNotificationEventDepartmentDto.notification
+							.conteudo,
+					rota: sendNotificationEventDepartmentDto.notification.rota,
+					data: sendNotificationEventDepartmentDto.notification.data,
+				},
+			});
+		} catch (err) {
+			throw new Error('Ocorreu um erro ao enviar a notificação');
+		}
 	}
 }

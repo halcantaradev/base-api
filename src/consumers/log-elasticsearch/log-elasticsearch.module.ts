978f452e75d9bd11e08826839b1cf669e52177c0
Helder Alcântara <a.helderalcatara@gmail.com>
2023-09-20 14:43:08 -0300

    Add autenticação elastic

--- src/consumers/log-elasticsearch/log-elasticsearch.module.ts
+++ src/consumers/log-elasticsearch/log-elasticsearch.module.ts
@@ -9,7 +9,13 @@ import { ElasticsearchModule } from '@nestjs/elasticsearch';
 	imports: [
 		FilaModule,
 		ConfigModule.forRoot(),
-		ElasticsearchModule.register({ node: 'http://localhost:9200' }),
+		ElasticsearchModule.register({
+			node: process.env.ELASTIC_NODE || 'http://localhost:9200',
+			auth: {
+				username: process.env.ELASTIC_USERNAME || 'elastic',
+				password: process.env.ELASTIC_PASSWORD || 'dev@1234',
+			},
+		}),
 	],
 	controllers: [LogElasticsearchController],
 	providers: [LogElasticsearchService],
--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@ yarn migrate:deploy
 yarn seed:prod
 pm2 start ./dist/src/sync-consumer.js 
 pm2 start ./dist/src/notification-consumer.js 
+pm2 start ./dist/src/log-elasticsearch-cosumer.js 
 pm2-runtime ./dist/src/main.js --no-autorestart

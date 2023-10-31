2beeed8b8fdbb1ba6ba2de1d22df009f741d77eb
Helder Alcântara <a.helderalcatara@gmail.com>
2023-10-31 13:06:12 -0300

    Correção busca de unidades

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -80,20 +80,21 @@ export class NotificationService {
 			},
 		});
 
-		await this.emailService.send({
-			from: process.env.EMAIL_SEND_PROVIDER,
-			html: `<p>Notificação criada para o condomino: ${condomino.nome} <br>
+		if (setupEmail)
+			await this.emailService.send({
+				from: process.env.EMAIL_SEND_PROVIDER,
+				html: `<p>Notificação criada para o condomino: ${condomino.nome} <br>
 			Clique no link para acessar clique: <a href="${url}">${url}</a></p>`,
-			subject: 'Notificação criada',
-			to: userLogado.email,
-			setup: {
-				MAIL_SMTP_HOST: setupEmail.host,
-				MAIL_SMTP_PORT: setupEmail.port,
-				MAIL_SMTP_SECURE: setupEmail.secure,
-				MAIL_SMTP_USER: setupEmail.user,
-				MAIL_SMTP_PASS: setupEmail.password,
-			},
-		});
+				subject: 'Notificação criada',
+				to: userLogado.email,
+				setup: {
+					MAIL_SMTP_HOST: setupEmail.host,
+					MAIL_SMTP_PORT: setupEmail.port,
+					MAIL_SMTP_SECURE: setupEmail.secure,
+					MAIL_SMTP_USER: setupEmail.user,
+					MAIL_SMTP_PASS: setupEmail.password,
+				},
+			});
 
 		return {
 			success: true,
